c5ade81b0b7b1d3266b93efd900ae1af29d14bf4
Daitarus <chajkovskij_99@mail.ru>
2023-07-17 18:09:01 +0300

    start

---
+++ TaskToStoreFactoryProducts/MainForm.cs
@@ -0,0 +1,12 @@
+namespace TaskToStoreFactoryProducts
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
