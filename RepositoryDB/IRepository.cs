258cbab1263ada297892c41be4158b7e5d23d359
Daitarus <chajkovskij_99@mail.ru>
2023-07-17 19:14:06 +0300

    add ConfigHandler and RepositoryDB

---
+++ RepositoryDB/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace RepositoryDB
+{
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+        void Remove(T entity);
+        IQueryable<T> SelectAll();
+        void SaveChanges();
+    }
+}
---
+++ RepositoryDB/Repository.cs
@@ -0,0 +1,32 @@
+﻿namespace RepositoryDB
+{
+    public class Repository<T> : IRepository<T> where T : Entity
+    {
+        protected DataBase database;
+        public Repository(DataBase database)
+        {
+            this.database = database;
+        }
+
+        public T? SelectForId(uint id)
+        {
+            return database.Find<T>(id);
+        }
+        public void Add(T entity)
+        {
+            database.Add(entity);
+        }        
+        public void Remove(T entity)
+        {
+            database.Remove(entity);
+        }
+        public IQueryable<T> SelectAll()
+        {
+            return database.Set<T>();
+        }
+        public void SaveChanges()
+        {
+            database.SaveChanges();
+        }
+    }
+}
